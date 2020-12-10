925b08b325031be275d466f1bc7aed9939ec4942
Jonathan Perry <jpper@umich.edu>
2020-12-10 15:04:53 +0900

    Finished up pretty cool tutorial (thanks Simon!)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import {
   AfterViewInit,
+  ChangeDetectorRef,
   Component,
   ElementRef,
   QueryList,
@@ -25,7 +26,10 @@ export class HomePage implements AfterViewInit {
 
   @ViewChildren(IonItem, { read: ElementRef }) items: QueryList<ElementRef>;
 
-  constructor(private gestureCtrl: GestureController) {}
+  constructor(
+    private gestureCtrl: GestureController,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   // View child elements are only available after viewinit
   ngAfterViewInit() {
@@ -50,22 +54,97 @@ export class HomePage implements AfterViewInit {
           oneItem.nativeElement.style.transition = "";
           oneItem.nativeElement.style.opacity = "0.8";
           oneItem.nativeElement.style.fontWeight = "bold";
+          // Disable scrolling the list
           this.contentScrollActive = false;
+          // Updates view if some updates aren't picked up
+          this.changeDetectorRef.detectChanges();
         },
         onMove: (ev) => {
           oneItem.nativeElement.style.transform = `translate(${ev.deltaX}px, ${ev.deltaY}px)`;
-          oneItem.nativeElement.style.zIndex = 10;
+          oneItem.nativeElement.style.zIndex = 11;
+          this.checkDropzoneHover(ev.currentX, ev.currentY);
         },
-        onEnd: (ev) => {},
+        onEnd: (ev) => {
+          this.contentScrollActive = true;
+          this.handleDrop(oneItem, ev.currentX, ev.currentY, i);
+        },
       });
       drag.enable();
       this.gestureArray.push(drag);
     }
 
     this.items.changes.subscribe((res) => {
-      console.log("items changed: ", res);
+      // console.log("items changed: ", res);
+      if (this.gestureArray.length != this.items.length) {
+        this.updateGestures();
+      }
     });
   }
 
   // Check if we are dragging above a dropzone
+  checkDropzoneHover(x, y) {
+    const dropA = this.dropA.nativeElement.getBoundingClientRect();
+    const dropB = this.dropB.nativeElement.getBoundingClientRect();
+
+    if (this.isInZone(x, y, dropA)) {
+      this.dropA.nativeElement.style.backgroundColor = "blue";
+    } else {
+      this.dropA.nativeElement.style.backgroundColor = "white";
+    }
+
+    if (this.isInZone(x, y, dropB)) {
+      this.dropB.nativeElement.style.backgroundColor = "red";
+    } else {
+      this.dropB.nativeElement.style.backgroundColor = "white";
+    }
+  }
+
+  // Check if coordinates are within a dropzone rect
+  isInZone(x, y, dropzone) {
+    if (x < dropzone.left || x >= dropzone.right) {
+      return false;
+    }
+    if (y < dropzone.top || y >= dropzone.bottom) {
+      return false;
+    }
+    return true;
+  }
+
+  // Decide what to do with dropped item
+  handleDrop(item, endX, endY, index) {
+    const dropA = this.dropA.nativeElement.getBoundingClientRect();
+    const dropB = this.dropB.nativeElement.getBoundingClientRect();
+
+    if (this.isInZone(endX, endY, dropA)) {
+      // Dropped in Zone A
+      const removedItem = this.myArray.splice(index, 1);
+      this.teamBlue.push(removedItem[0]);
+      item.nativeElement.remove();
+      // Sort to show the values in increasing order
+      this.teamBlue.sort(function (a, b) {
+        return a - b;
+      });
+    } else if (this.isInZone(endX, endY, dropB)) {
+      // Dropped in Zone B
+      const removedItem = this.myArray.splice(index, 1);
+      this.teamRed.push(removedItem[0]);
+      item.nativeElement.remove();
+      // Sort to show the values in increasing order
+      this.teamRed.sort(function (a, b) {
+        return a - b;
+      });
+    } else {
+      // Don't drop the item into a zone
+      // Simply bring it back to the initial position
+      item.nativeElement.style.transition = ".2s ease-out";
+      item.nativeElement.style.zIndex = "inherit";
+      item.nativeElement.style.transform = `translate(0,0)`;
+      item.nativeElement.style.opacity = "1";
+      item.nativeElement.style.fontWeight = "normal";
+    }
+
+    this.dropA.nativeElement.style.backgroundColor = "white";
+    this.dropB.nativeElement.style.backgroundColor = "white";
+    this.changeDetectorRef.detectChanges();
+  }
 }
